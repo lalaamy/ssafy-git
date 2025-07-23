bb601a4babf877f2b1e043e6ece3f6456cb0dce2
lalaamy <lalaamy@naver.com>
2025-07-23 17:41:25 +0900

    z

--- python/1681.py
+++ python/1681.py
@@ -3,8 +3,13 @@ number_of_people = 0
 print("현재 가입 된 유저 수 :", number_of_people)
 
 def increase_user():
-    pass
-
+    global number_of_people
+    number_of_people += 1
+    return number_of_people
 
 def create_user():
-    pass
+    user_data = {'name' : '홍길동', 'age' : '30', 'address' : '서울'}    
+    print (f"{user_data['name']}님 환영합니다!")
+create_user()
+
+print ("현재 가입 된 유저 수 :", increase_user())
---
+++ python/2215.py
@@ -0,0 +1,6 @@
+# 아래 함수를 수정하시오.
+def add_numbers(number1, number2):
+    add_numbers = number1 + number2
+    return add_numbers
+print(add_numbers(3,5))
+# 수정한 add_numbers() 함수를 호출하시오.
