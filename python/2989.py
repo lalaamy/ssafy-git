8728b65e752448ce9687f029b963a824d2832b34
lalaamy <lalaamy@naver.com>
2025-07-24 13:34:29 +0900

    z

---
+++ python/2989.py
@@ -0,0 +1,10 @@
+pro_num = 1000
+global_data = {'subject': 'python', 'day': 3, 'title': '함수 활용하기'}
+
+def create_data():
+    data = {}
+    return data
+
+result_1 = create_data()
+result_2 = create_data()
+result_3 = create_data()
