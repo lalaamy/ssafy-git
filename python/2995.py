6ed94994c4640f39ad8cd922e94b56a99cf4c581
lalaamy <7176ryu@naver.com>
2025-07-28 15:12:26 +0900

    2997

---
+++ python/2995.py
@@ -0,0 +1,18 @@
+N = 9
+data_1 = '123456789'
+arr_1 = []
+# 아래에 코드를 작성하시오.
+
+for i in range(len(data_1)) :
+    arr_1.append(data_1[i])
+print(arr_1)
+
+M = 15
+data_2 = '1 2 3 4 5 6 7 8 9 10 11 12 13 14 15'
+# 아래에 코드를 작성하시오.
+
+arr_2 = list(map(int, data_2.split()))
+
+for j in range(len(arr_2)) :
+    if j % 2 == 1 :
+        print(j)
---
+++ python/2996.py
@@ -0,0 +1,38 @@
+data_1 = 'qweqwYadnOyjnsaU4trwg asjnaAn245krRmkfE 42grTasdnHasdnvEasdn asdevadnBasdanEsdkqefqefvaSasdqaeeqqvedwt5hfbsdT24tewfd'
+'''
+예시코드
+arr = [1, 2, 3, 4, 5]
+for num in arr:
+    print(num, end='')
+출력결과 : 12345
+'''
+# 아래에 코드를 작성하시오.
+
+data_alpha = []
+
+for i in data_1 : 
+    if i.isupper() or i.isspace() :
+        data_alpha.append(i)
+print(''.join(data_alpha))
+
+
+data_2 = '걉파반샤팝다푸거맥파바자들퍼바배들밥샵파누타히매니배사바파힘다브사부힙헤베내테치대내'
+arr = []
+# 아래에 코드를 작성하시오.
+
+data_n = data_2.find("내")
+data_h = data_2.find("힘")
+data_dl = data_2.find("들")
+data_da = data_2.find("다")
+
+
+for j in ['내', '힘', '들', '다'] :
+    arr.append(data_2.find(j))
+print (arr)
+
+arr.sort()
+print(arr)
+
+for k in arr :
+    string = data_2[k]
+    print (string, end='')
