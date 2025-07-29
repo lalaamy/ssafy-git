ce016acc58ce3cd0d66ccfed2b6d1348cb70e993
lalaamy <7176ryu@naver.com>
2025-07-29 17:13:16 +0900

    1447

---
+++ python/1447.py
@@ -0,0 +1,23 @@
+# 아래 함수를 수정하시오.
+def union_sets(set1, set2):
+    return set1 | set2
+
+
+def union_multiple_sets(*sets):
+    if len(sets) == 1 :
+        print ("최소 두 개의 셋이 필요합니다.")
+    else :
+        sets_union = set()
+        for i in sets :
+            sets_union = sets_union.union(i)
+        return sets_union
+
+
+result = union_sets({1, 2, 3}, {3, 4, 5})
+print(result)  # {1, 2, 3, 4, 5}
+
+result = union_multiple_sets({1, 2}, {3, 4}, {5, 6})
+print(result)  # {1, 2, 3, 4, 5, 6}
+
+result = union_multiple_sets({1, 2})
+# 출력 : 최소 두 개의 셋이 필요합니다
---
+++ python/3000.py
@@ -0,0 +1,36 @@
+data = [
+    {
+        'name': 'galxy flip',
+        'company': 'samsung',
+        'is_collapsible': True,
+    },
+    {
+        'name': 'ipad',
+        'is_collapsible': False
+    },
+    {
+        'name': 'galxy fold',
+        'company': 'samsung',
+        'is_collapsible': True
+    },
+    {
+        'name': 'galxy note',
+        'company': 'samsung',
+        'is_collapsible': False
+    },
+    {
+        'name': 'optimus',
+        'is_collapsible': False
+    },
+]
+
+key_list = ['name', 'company', 'is_collapsible']
+
+# 아래에 코드를 작성하시오.
+
+
+for data_dict in data :
+    for key in key_list :
+        data_dict.setdefault(key, 'unknown')
+        value = data_dict.get(key)
+        print (f'{key} 은/는 {value} 입니다.')
