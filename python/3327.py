1e23c0752889be92fa9c087aede8e557249f937b
lalaamy <lalaamy@naver.com>
2025-07-30 17:54:18 +0900

    3327

--- python/3327.py
+++ python/3327.py
@@ -5,7 +5,6 @@ class Myth :
         self.type_of_myth += 1
         self.name = name
         
-
     @staticmethod
     def description() :
         print ("신화는 한 나라 혹은 한 민족으로부터 전승되어 오는 예로부터 섬기는 신을 둘러싼 이야기를 뜻한다.")
@@ -16,5 +15,5 @@ n2 = Myth('greek & rome')
 print(n1.name)
 print(n2.name)
 
-n1.type_of_myth()
-print(n1.type_of_myth)
+Myth.type_of_myth()
+print(Myth.type_of_myth)
